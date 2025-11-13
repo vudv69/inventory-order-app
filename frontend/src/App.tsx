a174423ffdbff5722240f06de64392eaac2b4220
vudv <vudv1993@gmail.com>
2025-11-13 18:07:37 +0700

    feat: remove github banner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
+import { Authenticated, Refine } from "@refinedev/core";
 import { KBarProvider } from "@refinedev/kbar";
 import {
   ErrorComponent,
@@ -61,7 +61,6 @@ const App: React.FC = () => {
 
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <KBarProvider>
         <ColorModeContextProvider>
           <CssBaseline />
